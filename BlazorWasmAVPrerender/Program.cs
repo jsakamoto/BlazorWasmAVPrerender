184ea0acdc3afcf4e4e5a4240b2034c1df54d8c1
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-11-02 08:08:55 +0530

    Add project files.

---
+++ BlazorWasmAVPrerender/Program.cs
@@ -0,0 +1,22 @@
+using BlazorWasmAVPrerender;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
+if (!builder.RootComponents.Any())
+{
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+}
+
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
+
+await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+}
+
