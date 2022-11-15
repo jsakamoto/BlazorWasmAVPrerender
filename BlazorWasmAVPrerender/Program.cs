fa86eff284c71fd6a82edd0338f7131bd57bfdce
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-11-16 00:08:04 +0530

    Add LazyAssemblyLoader Service

--- BlazorWasmAVPrerender/Program.cs
+++ BlazorWasmAVPrerender/Program.cs
@@ -1,6 +1,7 @@
 using BlazorWasmAVPrerender;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -18,5 +19,7 @@ await builder.Build().RunAsync();
 static void ConfigureServices(IServiceCollection services, string baseAddress)
 {
     services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+
+    services.AddScoped<LazyAssemblyLoader>();
 }
 
